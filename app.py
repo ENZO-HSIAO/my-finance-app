cf629abd9d1c2015d0cd19f2107f72c206c4c969
ENZO-HSIAO <work20019013@gmail.com>
2026-04-15 15:02:35 +0800

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 st.set_page_config(page_title="My Finance", page_icon="💰", layout="centered")
 
 # --- 2. 密碼保護功能 ---
-CORRECT_PASSWORD = "你的自訂密碼" 
+CORRECT_PASSWORD = "900612" 
 
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
